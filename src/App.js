f7816f3496c49a3383538dab4dc96b7bff406816
SteveReid <steve@appby.design>
2024-08-08 18:56:24 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,111 @@
-import logo from './logo.svg';
+import { Chart as ChartJS, defaults } from 'chart.js/auto';
+import { Bar, Doughnut, Line } from 'react-chartjs-2';
+
 import './App.css';
+
+import sourceData from './data/sourceData.json';
+import revenueData from './data/revenueData.json';
+
+defaults.maintainAspectRatio = false;
+defaults.responsive = true;
+
+defaults.plugins.title.display = true;
+defaults.plugins.title.align = 'start';
+defaults.plugins.title.font.size = 20;
+defaults.plugins.title.color = 'black';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="dataCard revenueCard">
+        <Line
+          data={{
+            labels: revenueData.map(data => data.label),
+            datasets: [
+              {
+                label: 'Revenue',
+                data: revenueData.map(data => data.revenue),
+                backgroundColor: 'rgba(43, 63, 229, 0.8)',
+                borderColor: 'rgba(43, 63, 229, 0.8)',
+              },
+              {
+                label: 'Cost',
+                data: revenueData.map(data => data.cost),
+                backgroundColor: 'rgba(250, 192, 19, 0.8)',
+                borderColor: 'rgba(250, 192, 19, 0.8)',
+              },
+            ],
+          }}
+          options={{
+            elements: {
+              line: {
+                tension: 0.5,
+              },
+            },
+            plugins: {
+              title: {
+                text: 'Monthly Revenue & Cost',
+              },
+            },
+          }}
+        />
+      </div>
+      <div className="dataCard customerCard">
+        <Bar
+          data={{
+            labels: sourceData.map(data => data.label),
+            datasets: [
+              {
+                label: 'Count',
+                data: sourceData.map(data => data.value),
+                backgroundColor: [
+                  'rgba(43, 63, 229, 0.8)',
+                  'rgba(250, 192, 19, 0.8)',
+                  'rgba(253, 135, 135, 0.8)',
+                ],
+                borderRadius: 5,
+              },
+            ],
+          }}
+          options={{
+            plugins: {
+              title: {
+                text: 'Revenue Source',
+              },
+            },
+          }}
+        />
+      </div>
+      <div className="dataCard categoryCard">
+        <Doughnut
+          data={{
+            labels: sourceData.map(data => data.label),
+            datasets: [
+              {
+                label: 'Count',
+                data: sourceData.map(data => data.value),
+                backgroundColor: [
+                  'rgba(43, 63, 229, 0.8)',
+                  'rgba(250, 192, 19, 0.8)',
+                  'rgba(253, 135, 135, 0.8)',
+                ],
+                borderColor: [
+                  'rgba(43, 63, 229, 0.8)',
+                  'rgba(250, 192, 19, 0.8)',
+                  'rgba(253, 135, 135, 0.8)',
+                ],
+              },
+            ],
+          }}
+          options={{
+            plugins: {
+              title: {
+                text: 'Revenue Sources',
+              },
+            },
+          }}
+        />
+      </div>
     </div>
   );
 }
